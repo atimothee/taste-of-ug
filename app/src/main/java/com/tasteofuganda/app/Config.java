5bc3902eac213f6f50d5cda23313cb4153d8b3c9
Timothy Asiimwe <atimothee@gmail.com>
2014-12-24 22:20:21 +0300

    deleted useless gcm helper & config classes

--- app/src/main/java/com/tasteofuganda/app/Config.java
+++
@@ -1,9 +0,0 @@
-package com.tasteofuganda.app;
-
-/**
- * Created by Timo on 12/24/14.
- */
-public class Config {
-    //Google Developers Console project number
-    private static final String SENDER_ID = "10592780844";
-}
